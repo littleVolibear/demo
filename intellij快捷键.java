ce56b170b6da486e0e771bcd79de10226ea9eeed
volibearGeek <liuyongtao@imxiaomai.com>
2018-09-30 15:20:15 +0800

    update

--- intellij快捷键.java
+++ intellij快捷键.java
@@ -21,48 +21,28 @@ Ctrl＋F7:可以查询当前所有的相同元素，然后按F3可以选择
 alt+f7:查询该变量在当前文件所有位置引用及数量
 Alt+F3:逐个往下查找相同文本，并高亮显示
 ctrl+F3:跳转到词的下个引用位置
-批量修改变量名:shift + F6
+shift + F6:批量修改变量名
 alt + j :增量选择下个相同单词
 ctrl + alt + 左方向键: 退回到上次编辑位置
-Ctrl+Shift+Space，自动补全代码
-Shift+Enter 插入空行
+Ctrl+Shift+Space:自动补全代码
+Shift+Enter:插入空行
 ctrl+shift+M:选中对应括号
-Ctrl + Shift + Backspace:回到最后编辑位置
+Ctrl+Shift+Backspace:回到最后编辑位置
+Alt+Shift+Up/Down:上/下移一行
+Ctrl+Shift+F7:高亮显示所有该文本
+Alt+F3:逐个往下查找相同文本，并高亮显示
+alt+7:查看structure视图
+ctrl+H:类继承机构
+ctrl+alt+H:类和方法被调用位置
+ctrl+shift+enter:帮你自动填充分号
+Ctrl+Alt+O:优化导入的类和包
 
-Alt+Shift+Up/Down，上/下移一行
-Ctrl+X，删除行
-Ctrl+Shift+F7，高亮显示所有该文本，按 Esc 高亮消失
-Alt+F3，逐个往下查找相同文本，并高亮显示
-alt+7 查看structure视图
-
-
-
-Ctrl+Enter，导入包，自动修正
 
 Ctrl+Alt+I，将选中的代码进行自动缩进编排，这个功能在编辑 JSP 文件时也可以工作
-Ctrl+Alt+O，优化导入的类和包
-Ctrl+R，替换文本
-
 Ctrl+空格，代码提示（与系统输入法快捷键冲突）
 Ctrl+Shift+Alt+N，查找类中的方法或变量
 Alt+Shift+C，最近的更改
-Alt+Shift+Up/Down，上/下移一行
-
-Ctrl+H，显示类结构图（类的继承层次）
-Alt+F1，查找代码所在位置
-
-
 自动补全属性名称 : Ctrl + Alt + f
-
-
-跳到类或方法的声明         ctr + B
-
-查看类的继承结构             ctr + H
-查看方法的继承结构          ctr + shift + H
-查看类或方法被调用情况  ctr + alt +H 
-原地参看类、方法的声明 Ctrl + Shift + I
-
-
 itar 生成array 
 iter 生成增强forxun
 itli 生成List的遍历
@@ -70,23 +50,9 @@ itit  生成iterator 迭代(while)
 
 
 ctrl + i  实现接口的方法
-
-
-IntelliJ Idea 常用快捷键列表
-
-Ctrl+Alt+t 选择代码块 try catch
-
 Alt+回车 导入包,自动修正
-Ctrl+N   查找类
 Ctrl+Shift+N 查找文件
-Ctrl+Alt+L  格式化代码
-Ctrl+Alt+O 优化导入的类和包
-Alt+Insert 生成代码(如get,set方法,构造函数等) mac系统 fn+alt+回车
-Ctrl+E或者Alt+Shift+C  最近更改的代码
 Ctrl+R 替换文本
-Ctrl+F 查找文本
-Ctrl+Shift+Space 自动补全代码
-Ctrl+空格 代码提示
 Ctrl+Alt+Space 类名或接口名提示
 Ctrl+P 方法参数提示
 Ctrl+Shift+Alt+N 查找类中的方法或变量
@@ -94,23 +60,18 @@ Alt+Shift+C 对比最近修改的代码
 
 Ctrl+Alt+H打开方法调用结构    >>> 
 
-Shift+F6  重构-重命名
-Ctrl+Shift+先上键
-Ctrl+/ 或 Ctrl+Shift+/  注释（// 或者/*...*/ ）
 Ctrl+J  自动代码
-Ctrl+E 最近打开的文件
 Ctrl+H 显示类结构图
-Ctrl+Q 显示注释文档
+Alt+F1 查找代码所在位置
+Ctrl+Alt+ left/right 返回至上次浏览的位置
+Alt+ left/right 切换代码视图
+Alt+ Up/Down 在方法间快速移动定位
+Ctrl+Shift+Up/Down 代码向上/下移动。
+ 
+=================================
+Ctrl+H 显示类结构图
 Alt+F1 查找代码所在位置
 Alt+1 快速打开或隐藏工程面板
 Ctrl+Alt+ left/right 返回至上次浏览的位置
 Alt+ left/right 切换代码视图
 Alt+ Up/Down 在方法间快速移动定位
-Ctrl+Shift+Up/Down 代码向上/下移动。
-F2 或Shift+F2 高亮错误或警告快速定位
- 
-代码标签输入完成后，按Tab，生成代码。
-选中文本，按Ctrl+Shift+F7 ，高亮显示所有该文本，按Esc高亮消失。
-选中文本，按Alt+F3 ，逐个往下查找相同文本，并高亮显示。
-Ctrl+Up/Down 光标跳转到第一行或最后一行下
-Ctrl+B 快速打开光标处的类或方法 
